4b9f313c4ad5051d17efef78340b9ec815048ec3
Evghenii <evghenii.sirbu@yahoo.com>
2023-09-27 17:37:25 +0200

    added chart components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import ReactApexChart from 'react-apexcharts';
 
 function App() {
+  // Sample data for demonstration purposes
+  const chartData = {
+    categories: ['Category 1', 'Category 2', 'Category 3'],
+    series: [{
+      name: 'Series 1',
+      data: [30, 40, 20],
+    }],
+  };
+
+  // State to manage chart options and data
+  const [chartOptions, setChartOptions] = useState({});
+  const [chartSeries, setChartSeries] = useState([]);
+
+  useEffect(() => {
+    // Prepare chart options (customize as needed)
+    const options = {
+      chart: {
+        type: 'bar',
+      },
+      xaxis: {
+        categories: chartData.categories,
+      },
+    };
+
+    // Set chart options
+    setChartOptions(options);
+
+    // Set chart series data
+    setChartSeries(chartData.series);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data Visualization</h1>
+      <div className="chart">
+        <ReactApexChart options={chartOptions} series={chartSeries} type="bar" />
+      </div>
     </div>
   );
 }
